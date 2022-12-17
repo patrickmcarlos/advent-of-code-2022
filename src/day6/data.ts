6b6363221d471b34e7fb74d63cc207dbaf971f42
Patrick Carlos <patrickmcarlos@hotmail.com>
2022-12-16 22:52:20 -0800

    complete day 6 pt 1

---
+++ src/day6/data.ts
@@ -0,0 +1,8 @@
+// 5
+export const EXAMPLE_ONE = `bvwbjplbgvbhsrlpgdmjqwftvncz`;
+
+export const EXAMPLE_TWO = `nppdvjthqldpwncqszvftbrmjlhg`;
+
+export const EXAMPLE_THREE = `nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg`;
+
+export const EXAMPLE_FOUR = `zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw`;
---
+++ src/day6/day6_pt1.ts
@@ -0,0 +1,29 @@
+import { EXAMPLE_FOUR, EXAMPLE_ONE, EXAMPLE_THREE, EXAMPLE_TWO } from './data';
+
+
+// Sliding window
+// for every substring, put it into a set
+// on the first set that has a size of 4, we take the 2nd pointer idx as the result
+const findNumCharactersToFirstMarker = (input: string, windowSize: number) => {
+    let [left, right] = [0, windowSize];
+    let ans = 0;
+
+    while (right < input.length) {
+        const set = new Set<string>(input.slice(left, right));
+
+        if (set.size === windowSize) {
+            ans = right;
+            break;
+        }
+
+        left++;
+        right++;
+    }
+
+    return ans;
+};
+
+console.log(findNumCharactersToFirstMarker(EXAMPLE_ONE, 4));
+console.log(findNumCharactersToFirstMarker(EXAMPLE_TWO, 4));
+console.log(findNumCharactersToFirstMarker(EXAMPLE_THREE, 4));
+console.log(findNumCharactersToFirstMarker(EXAMPLE_FOUR, 4));
